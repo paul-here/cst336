b363153d27fd757ac34880ab04d0e880a06fd2e5
Paul Whipp <pwhipp@csumb.edu>
2019-03-09 23:07:43 +0000

    initial copy from Dr. Lara

---
+++ labs/lab6/getProducts.php
@@ -0,0 +1,21 @@
+<?php
+
+// Database connection (there are 3 ways)
+// Here is one of them: PDO
+
+$host = "localhost";
+$username = "root";
+$password = "";
+// Establishing the connection
+$dbConn = new PDO("mysql:host=$host;dbname=$dbname;", $username, $password);
+// Printing out any errors: (see powerpoint and snag the line yooooo)
+
+
+$sql = "SELECT * FROM om_product ORDER BY productPrice LIMIT 10";
+
+
+$stmt = $dbConn -> prepare($sql);
+$stmt -> execute();
+
+
+?>
