b8916fc83475cc60e30f46eefb007fc3b2210b84
Paul Whipp <pwhipp@csumb.edu>
2019-04-12 02:12:05 +0000

    db tests

--- labs/lab6/api/getProducts.php
+++ labs/lab6/api/getProducts.php
@@ -16,7 +16,7 @@ $stmt = $dbConn -> prepare($sql);  //$connection MUST be previously initialized
 $stmt->execute();
 $records = $stmt->fetchAll(PDO::FETCH_ASSOC); //use fetch for one record, fetchAll for multiple
 
-//print_r($records); //displays array content
+print_r($records); //displays array content
 
 echo json_encode($records);
 
