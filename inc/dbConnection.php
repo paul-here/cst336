d7636a391ea790204a1c8e58201af81c1a1f5beb
Paul Whipp <pwhipp@csumb.edu>
2019-04-13 18:44:14 +0000

    synced with Dr. Lara's workspace

--- inc/dbConnection.php
+++ inc/dbConnection.php
@@ -1,23 +1,24 @@
 <?php
 
-    function getDatabaseConnection($dbname) {
+    function getDatabaseConnection($dbName) {
     
-        $host = 'localhost';
-        $username = 'root';
-        $password = '';
+        $host = "localhost";
+        $username = "root";
+        $password = "";
         
         //checks whether the URL contains "herokuapp" (using Heroku)
         if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
            $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
            $host = $url["host"];
-           $dbname = substr($url["path"], 1);
+           $dbName = substr($url["path"], 1);
            $username = $url["user"];
            $password = $url["pass"];
         }
         
-        $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $dbConn = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
         $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
         
         return $dbConn;
+    
     }
 ?>
