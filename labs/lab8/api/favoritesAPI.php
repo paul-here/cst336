c8c7195c365201db3ea8b48cd5f3e8661901a599
Paul Whipp <pwhipp@csumb.edu>
2019-04-19 00:09:09 +0000

    test push

---
+++ labs/lab8/api/favoritesAPI.php
@@ -0,0 +1,23 @@
+<?php
+
+//receives these parameters: action, url, keyword
+
+//TO GET THE 2 EXTRA POINTS IN THE HANDS-ON PORTION OF THE FINAL EXAM
+//1. Add favorites to database
+//2. Remove favorites from database
+//3. Display the keyword list from database (use DISTINCT)
+
+
+  switch (action) {
+      
+      case "add":
+                 break;
+                 
+
+      case "delete":
+                 break;
+                 
+      
+  }//switch
+
+?>
