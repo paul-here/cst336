cd86f0587d520db6e8dabb2181935df1b291f8e3
Paul Whipp <pwhipp@csumb.edu>
2019-04-24 04:34:23 +0000

    final testing

--- labs/lab8/api/favoritesAPI.php
+++ labs/lab8/api/favoritesAPI.php
@@ -3,9 +3,9 @@
 include '../../../inc/dbConnection.php';
 $conn = getDatabaseConnection("c9");
 //receives these parameters: action, url, keyword
-$action = $_POST['action'];
-$url = $_POST['url'];
-$keyword = $_POST['keyword'];
+$action = $_GET['action'];
+$url = $_GET['url'];
+$keyword = $_GET['keyword'];
 
 //TO GET THE 2 EXTRA POINTS IN THE HANDS-ON PORTION OF THE FINAL EXAM
 //1. Add favorites to database
@@ -14,7 +14,7 @@ $keyword = $_POST['keyword'];
 
 $arr = array();
 
-  switch ($action) {
+switch ($action) {
       
       case "add":
         $arr[":keyword"] = $keyword;
@@ -40,13 +40,14 @@ $arr = array();
                  
       case "favorites":
         $arr[":keyword"] = $keyword;
-        $sql = "SELECT * FROM `lab8_pixabay` WHERE `lab8_pixabay`.`imageURL` = :keyword";
+        $sql = "SELECT `imageURL` FROM `lab8_pixabay` WHERE `lab8_pixabay`.`keyword` = :keyword";
         $stmt = $conn->prepare($sql);
         $stmt->execute($arr);
+                 break;
                  
       default:
                  break;
-  }//switch
+  } // switch
   
   $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
   echo json_encode($records);
--- labs/lab8/viewFavorites.php
+++ labs/lab8/viewFavorites.php
@@ -1,22 +1,106 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <meta charset="utf-8" />
         <title>Pixabay API Favorites</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="css/styles.css" type="text/css" />
         <script>
         /* global $ */
-        $(document).ready(function() {
-
-                alert("test");
-                $.ajax({
-                    method: "POST",
+        
+        function createActionButton(keyword){
+            
+            let htmlStr = "<button onclick='displayFavorites(this.innerHTML)'>";
+            htmlStr += keyword;
+            htmlStr += "</button>";
+            
+            return htmlStr;
+        }
+        
+        function displayFavorites(keyword){
+            
+            $.ajax({
+                    method: "GET",
                     url: "api/favoritesAPI.php",
                     dataType: "json",
-                    data: { "action" : "favorites", "url" : "", "keyword" : ""},
+                    data: { "action" : "favorites", "url" : "", "keyword" : keyword},
                     success: function(data, status) {
-                        alert("success");
+                        
+                        let htmlString = "";
+                        let newRow = 0
+                        $("#images").html("");
+                        data.forEach(function(ele){
+                            
+                            if(newRow == 0){
+                                htmlString += "<div class='row'>";
+                            }
+                            
+                            htmlString += "<img class='favorite' src='img/favorite_on.png' width='30'>";
+                            htmlString += "<img src='" + ele['imageURL'] + "' width='300' height='280'>";
+                            
+                            if(newRow == 2){
+                                newRow = -1;
+                                htmlString += "</div>";
+                            }
+                            
+                            newRow++;
+                        });
+                        
+                    $("#images").html(htmlString);
+
                     }
                 }); //ajax 
+        }
+        
+        $(document).ready(function() {
+
+                $.ajax({
+                    method: "GET",
+                    url: "api/favoritesAPI.php",
+                    dataType: "json",
+                    data: { "action" : "keyword", "url" : "", "keyword" : ""},
+                    success: function(data, status) {
+                        let newRow = 0
+                        data.forEach(function(ele){
+                            newRow++;
+                            $("#links").append(createActionButton(ele['keyword']));
+                            if(newRow > 7){
+                                $("#links").append("<br>");    
+                                newRow = 0;
+                            }
+                        });
+                    }
+                }); //ajax 
+                
+                $("#images").on("click", ".favorite", function() {
+
+                    if ($(this).attr("src") == "img/favorite.png") {
+
+                    $(this).attr("src", "img/favorite_on.png");
+                    //add image url to database
+                    callFavoriteAPI("add", $(this).next().attr("src"), $("#keyword").val());
+                }
+                else {
+
+                    $(this).attr("src", "img/favorite.png");
+                    //remove image url from database
+                    callFavoriteAPI("delete", $(this).next().attr("src"), $("#keyword").val());
+
+                }
+                
+                function callFavoriteAPI(action, url, keyword) {
+
+                $.ajax({
+                    method: "GET",
+                    url: "api/favoritesAPI.php",
+                    dataType: "json",
+                    data: { "action": action, "url": url, "keyword": keyword },
+                    success: function(data, status) {
+
+                    }
+                }); //ajax 
+            }
+            });
 
         }); //documentReady
     </script>
@@ -24,6 +108,13 @@
     <body>
 
         <h1> Favorite Images </h1>
+        <br>
+        <div id="links"></div>
+        <div id="images"></div>
+        <br>
+        <form action="index.html">
+            <button> Return </button>
+        </form>
         
     </body>
 </html>
